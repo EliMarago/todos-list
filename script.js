1a951207aaa6393fc4e8827c2aa490b8b941b7d0
Elisabetta <156212818+EliMarago@users.noreply.github.com>
2025-10-25 16:26:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,59 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todoUL = document.getElementById("todos");
-
 const todosStorage = JSON.parse(localStorage.getItem("todos"));
-
 if (todosStorage && Array.isArray(todosStorage)) {
   todosStorage.forEach((todo) => {
     addTodo(todo);
   });
 }
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   addTodo();
 });
-
 function addTodo(todo) {
   let todoText = input.value;
-
   if (todo) {
     todoText = todo.text;
   }
   if (todoText) {
     const todoEl = document.createElement("li");
-    todoEl.appendChild(document.createTextNode(todoText));
+    const textNode = document.createTextNode(todoText);
+    todoEl.appendChild(textNode);
     const icon = document.createElement("i");
+
     icon.classList.add("fa", "fa-trash");
-    icon.style.marginLeft = "10px";
-    icon.style.cursor = "pointer";
 
     todoEl.appendChild(icon);
+
+    icon.addEventListener("click", (e) => {
+      e.stopPropagation();
+      todoEl.remove();
+      updateLS;
+    });
     if (todo && todo.completed) {
       todoEl.classList.add("completed");
     }
-    todoEl.innerText = todoText;
 
-    todoEl.addEventListener("click", () => {
+    todoEl.addEventListener("click", (e) => {
+      if (e.target.tagName.toLowerCase() === "i") return;
       todoEl.classList.toggle("completed");
-      uppdateLS;
+
+      updateLS;
     });
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
       todoEl.remove();
-      uppdateLS();
+      updateLS();
     });
     todoUL.appendChild(todoEl);
     input.value = "";
-
-    uppdateLS();
+    updateLS();
   }
 }
-
-function uppdateLS() {
+function updateLS() {
   const todosEl = document.querySelectorAll("li");
-
   const todo = [];
-
   todosEl.forEach((todoEl) => {
     todo.push({
       text: todoEl.innerText,
@@ -64,7 +61,4 @@ function uppdateLS() {
     });
   });
   localStorage.setItem("todos", JSON.stringify(todo));
-}
-
-// localStorage.setItem("name", JSON.stringify(obj));
-// JSON.parse(localStorage.getItem(obj));
+} 
